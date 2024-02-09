f66a3aafa591b786d6013452209bc9756ce9af0a
@hexlet/cli <support@hexlet.io>
2024-02-09 11:50:30 +0400

    submit rails-basics-ru/i18n

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -2,7 +2,14 @@
 
 Rails.application.routes.draw do
   # BEGIN
-  
+  scope "(:locale)", locale: /en|ru/ do
+    root 'home#index'
+    resources :posts do
+      scope module: :posts do
+        resources :comments, only: %i[edit create update destroy]
+      end
+    end
+  end
   # END
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
