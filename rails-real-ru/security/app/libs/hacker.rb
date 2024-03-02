c7cde831f810e4f93b570586b225acede5c2ddef
@hexlet/cli <support@hexlet.io>
2024-03-02 14:18:36 +0400

    submit rails-real-ru/security

--- rails-real-ru/security/app/libs/hacker.rb
+++ rails-real-ru/security/app/libs/hacker.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'open-uri'
+require 'nokogiri'
+require 'net/http'
+require 'net/https'
 
 class Hacker
   class << self
@@ -11,30 +14,40 @@ class Hacker
       post_path = '/users'
 
       uri = URI(hostname)
-
-      uri.path = get_path
-
-      res = uri.open()
-      html = Nokogiri::HTML(res.read)
-
-      cookie = res.meta['set-cookie'].split(';')[0]
-      # csrf_token = html.at('meta[name="csrf-token"]')['content']
-
-      authenticity_token = html.at('input[name="authenticity_token"]')['value']
-
-      params = { 'user[email]' => email, 'user[password]' => password, 'user[password_confirmation]' => password, authenticity_token: }
-
-      uri.path = post_path
-      req = Net::HTTP::Post.new(uri)
-      req.body = URI.encode_www_form(params)
-      req['Cookie'] = cookie
-      # req['X-Csrf-Token'] = csrf_token
-
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = uri.scheme == 'https'
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      http.request req
+      req = Net::HTTP::Get.new URI.join(hostname, get_path)
+
+      res = http.request req
+
+      cookie = res.response['set-cookie'].split('; ')[0]
+      # csrf_token = html.at('meta[name="csrf-token"]')['content']
+
+      params = {
+        'user[email]': email,
+        'user[password]': password,
+        'user[password_confirmation]': password,
+        authenticity_token: auth_token(res.body)
+      }
+
+      req = Net::HTTP::Post.new URI.join(hostname, post_path)
+      req.body = URI.encode_www_form(params)
+      req['Cookie'] = cookie
+      # req['X-Csrf-Token'] = csrf_token
+
+      res = http.request req
+
+      res.code == '302'
+    end
+
+    private
+
+    def auth_token(body)
+      html = Nokogiri::HTML(body)
+
+      html.at('input[@name="authenticity_token"]')['value']
     end
     # END
   end
