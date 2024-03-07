91cd025f3897fe206f03699255950bd2982f9281
Nikita Konovalov <mipo4t@gmail.com>
2024-03-07 08:25:54 +0400

    start acl

--- rails-real-ru/nested-forms/app/models/resume.rb
+++ rails-real-ru/nested-forms/app/models/resume.rb
@@ -8,6 +8,7 @@ class Resume < ApplicationRecord
   has_many :educations, class_name: 'ResumeEducation', inverse_of: :resume, dependent: :destroy
   has_many :works, class_name: 'ResumeWork', inverse_of: :resume, dependent: :destroy
 
-  accepts_nested_attributes_for :educations, :works
+  accepts_nested_attributes_for :educations, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :works, reject_if: :all_blank, allow_destroy: true
   # END
 end
