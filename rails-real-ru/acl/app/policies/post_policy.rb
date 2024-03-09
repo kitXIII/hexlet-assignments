5c2e19dd04975541e6b150a6653a2114daa6ee4c
@hexlet/cli <support@hexlet.io>
2024-03-09 14:24:12 +0400

    submit rails-real-ru/acl

--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -2,6 +2,24 @@
 
 class PostPolicy < ApplicationPolicy
   # BEGIN
-  
+  def create?
+    user
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user == record.author || user&.admin
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user&.admin
+  end
   # END
 end
