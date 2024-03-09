c3a576a733cc65e1d9782544f0be800c4d3dab0d
@hexlet/cli <support@hexlet.io>
2024-03-09 14:36:06 +0400

    submit rails-real-ru/acl

--- rails-real-ru/acl/db/schema.rb
+++ rails-real-ru/acl/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2021_10_02_100641) do
+ActiveRecord::Schema[7.0].define(version: 2021_10_02_100641) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body"
