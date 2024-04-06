ef5b738a1f19dcf5a04533fa7a9b8bc17085c8b0
@hexlet/cli <support@hexlet.io>
2024-04-06 17:01:49 +0400

    submit rails-full-ru/api

--- rails-full-ru/api/app/controllers/api/users_controller.rb
+++ rails-full-ru/api/app/controllers/api/users_controller.rb
@@ -3,13 +3,13 @@
 class Api::UsersController < Api::ApplicationController
   # BEGIN
   def index
-    @users = User.all
-    respond_with @users.as_json
+    users = User.all
+    respond_with users
   end
 
   def show
-    @user = User.find params[:id]
-    respond_with @user, root: true
+    user = User.find params[:id]
+    respond_with user, root: true
   end
   # END
 end
