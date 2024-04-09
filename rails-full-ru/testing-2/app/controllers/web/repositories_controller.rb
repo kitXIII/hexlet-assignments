6cef64a6faa00cf8a97ea8372554eb3626452067
Nikita Konovalov <mipo4t@gmail.com>
2024-04-09 23:11:11 +0400

    start testing2

---
+++ rails-full-ru/testing-2/app/controllers/web/repositories_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# BEGIN
+
+# END
+
+class Web::RepositoriesController < Web::ApplicationController
+  def index
+    @repositories = Repository.all
+  end
+
+  def new
+    @repository = Repository.new
+  end
+
+  def show
+    @repository = Repository.find params[:id]
+  end
+
+  def create
+    # BEGIN
+    
+    # END
+  end
+
+  def edit
+    @repository = Repository.find params[:id]
+  end
+
+  def update
+    @repository = Repository.find params[:id]
+
+    if @repository.update(permitted_params)
+      redirect_to repositories_path, notice: t('success')
+    else
+      flash[:notice] = t('fail')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @repository = Repository.find params[:id]
+
+    if @repository.destroy
+      redirect_to repositories_path, notice: t('success')
+    else
+      redirect_to repositories_path, notice: t('fail')
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:repository).permit(:link)
+  end
+end
