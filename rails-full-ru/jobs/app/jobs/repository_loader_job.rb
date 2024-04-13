fba4cfdbfe983fb1ef33d0ed3566b7c3bb844680
@hexlet/cli <support@hexlet.io>
2024-04-13 21:59:22 +0400

    submit rails-full-ru/jobs

--- rails-full-ru/jobs/app/jobs/repository_loader_job.rb
+++ rails-full-ru/jobs/app/jobs/repository_loader_job.rb
@@ -8,29 +8,27 @@ class RepositoryLoaderJob < ApplicationJob
 
     return if repository.blank?
 
-    begin
-      repository.fetch!
+    repository.fetch!
 
-      client = Octokit::Client.new
+    client = Octokit::Client.new
 
-      octokit_repo = Octokit::Repository.from_url(repository.link)
+    octokit_repo = Octokit::Repository.from_url(repository.link)
 
-      github_data = client.repository(octokit_repo)
+    github_data = client.repository(octokit_repo)
 
-      repository.update!(
-        repo_name: github_data[:name],
-        owner_name: github_data[:owner][:login],
-        description: github_data[:description],
-        default_branch: github_data[:default_branch],
-        watchers_count: github_data[:watchers_count],
-        language: github_data[:language],
-        repo_created_at: github_data[:created_at],
-        repo_updated_at: github_data[:updated_at]
-      )
+    repository.update!(
+      repo_name: github_data[:name],
+      owner_name: github_data[:owner][:login],
+      description: github_data[:description],
+      default_branch: github_data[:default_branch],
+      watchers_count: github_data[:watchers_count],
+      language: github_data[:language],
+      repo_created_at: github_data[:created_at],
+      repo_updated_at: github_data[:updated_at]
+    )
 
-      repository.done! if repository.may_done?
-    rescue StandardError
-      repository.fail! if repository.may_fail?
-    end
+    repository.done! if repository.may_done?
+  rescue StandardError
+    repository.fail! if repository.may_fail?
   end
 end
